f76f9f77d39e933f3238236658fe64be1e73d60b
Samarth1-glitch <nsamarth2006@gmail.com>
2025-04-17 21:32:36 +0530

    api.js

---
+++ services/api.js
@@ -0,0 +1,15 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://64xxx.mockapi.io/appointments"
+
+export const getAppointments = async () => {
+  const res = await fetch(`${BASE_URL}/appointments`)
+  return res.json()
+}
+
+export const addAppointment = async (appointment) => {
+  const res = await fetch(`${BASE_URL}/appointments`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(appointment)
+  })
+  return res.json()
+}
